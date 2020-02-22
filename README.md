13f1e021ed941df23358f8f0fbf1e9fa4f9af956
Sayeed Anwar Syed Kamal <48358997+Sa7eed@users.noreply.github.com>
2020-02-22 02:09:40 -0500

    Commiting the files

---
+++ README.md
@@ -0,0 +1,1 @@
+# The program requires a Wolfram Alpha ID. If you don't have one then you must first create an Wolfram Alpha ID to use the code.
---
+++ main_file.py
@@ -0,0 +1,18 @@
+import wikipedia
+import wolframalpha
+
+print("Hi there, how may I help you?")
+query_type = str(input("a. for math and science related questions - 1\nb. for general topics - 2\n>"))
+my_input = str(input("question: "))
+
+if query_type == '1':
+    #wolframalpha
+    app_id = "Wolfram Alpha App ID"
+    client = wolframalpha.Client(app_id)
+    res = client.query(my_input)
+    answer = next(res.results).text
+    print(answer)
+        
+elif query_type == '2':
+    #wikipedia
+    print(wikipedia.summary(my_input))
